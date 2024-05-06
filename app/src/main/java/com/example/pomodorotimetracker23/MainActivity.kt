9b99005a5fc2e90ad5fd5f5a6e6641940571b37a
ManuelGerke <gerepo@web.de>
2024-05-06 14:15:05 +0200

    Initiate Project

--- app/src/main/java/com/example/pomodorotimetracker23/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimetracker23/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PomodoroTimeTracker23Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
